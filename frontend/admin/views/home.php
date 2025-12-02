ae2e34e454856e969edb89c32d21c247231f90de
Harutoo <herconaryangga15@gmail.com>
2025-12-02 09:23:26 +0700

    Forgot another one lel

--- frontend/admin/views/home.php
+++ frontend/admin/views/home.php
@@ -49,6 +49,7 @@
             min-height: 100vh;
             display: flex;
             justify-content: center;
+            align-items: flex-start;
             color: #fff;
             padding: 2rem;
         }
--- frontend/admin/views/members_form.php
+++ frontend/admin/views/members_form.php
@@ -50,6 +50,7 @@
             min-height: 100vh;
             display: flex;
             justify-content: center;
+            align-items: flex-start;
             color: #fff;
             padding: 2rem;
         }
