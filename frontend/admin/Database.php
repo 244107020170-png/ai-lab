bec07e4b6abd5d4c399207206436f310b5296088
244107020170-png <244107020170@student.polinema.ac.id>
2025-12-14 22:54:10 +0700

    pass

--- frontend/admin/Database.php
+++ frontend/admin/Database.php
@@ -10,7 +10,7 @@ class Database
             $port = "5432";
             $db = "ai_lab_db";     // ganti
             $user = trim("postgres");
-            $password = "1234";  // ganti
+            $password = "nasywa1010";  // ganti
             $connStr = "host=$host port=$port dbname=$db user=$user password=$password";
 
             self::$conn = pg_connect($connStr);
